506e8d377360a3dea1b44348ec3f9bc440df9f20
camnewnham <camnewnham@gmail.com>
2023-01-05 02:12:47 +1100

    change pins to 0 and 2 - note 0 must be d/c during flash

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,11 @@
 #include <SoftwareSerial.h>
 
 #define EN_PIN 12           // Driver Enable pin
-#define LED_PIN 2           // D1 Mini Hardware LED
 #define DRIVER_ADDRESS 0b00 // TMC2209 Driver address
 #define R_SENSE 0.11f       // SilentStepStick series use 0.11
 
-#define SW_RX 14 // Software RX -- must be interrupt capable
-#define SW_TX 12 // Software TX
+#define SW_RX 0 // Software RX -- must be interrupt capable
+#define SW_TX 2 // Software TX
 
 #define MICROSTEPS 4    // Microstepping
 #define RMS_CURRENT 636 // Motor RMS current in MA. My stepper is 42SHDC3025-24B which has peak current 0.9A = 636mA RMS
@@ -32,6 +31,7 @@ void setup()
 
   driver.toff(5);
   driver.microsteps(16);
+  driver.rms_current(RMS_CURRENT);
   driver.VACTUAL(1000);
 }
 
