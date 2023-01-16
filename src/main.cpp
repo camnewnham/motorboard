56bb962ba35162ff0561be47e0c7d8629de80e51
camnewnham <camnewnham@gmail.com>
2023-01-16 20:42:18 +1100

    working stallguard interrupt

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,34 @@
 #include <Arduino.h>
 #include <TMCStepper.h>
 #include <SoftwareSerial.h>
+#include <Ticker.h>
 
-#define EN_PIN 12           // Driver Enable pin
+#define EN_PIN 15           // Driver Enable pin
 #define DRIVER_ADDRESS 0b00 // TMC2209 Driver address
 #define R_SENSE 0.11f       // SilentStepStick series use 0.11
 
-#define SW_RX 0 // Software RX -- must be interrupt capable
-#define SW_TX 2 // Software TX
+#define SW_RX 2 // Software RX -- must be interrupt capable
+#define SW_TX 0 // Software TX
 
-#define MICROSTEPS 4    // Microstepping
+#define DIAG_PIN 13 // MAY NEED TO SWAP WITH 12 DEPENDING ON BOARD CONF
+
+#define STALL_VALUE     100 // [0..255]
+
+#define MICROSTEPS 0    // Microstepping
 #define RMS_CURRENT 636 // Motor RMS current in MA. My stepper is 42SHDC3025-24B which has peak current 0.9A = 636mA RMS
 
 SoftwareSerial softwareSerial(SW_RX, SW_TX);
 TMC2209Stepper driver(&softwareSerial, R_SENSE, DRIVER_ADDRESS); // Create TMC driver
+
+Ticker resetter;
+
+bool interrupted = false;
+
+bool shaft = false;
+
+void IRAM_ATTR onStall() {
+    interrupted = true;
+}
 
 void setup()
 {
@@ -25,22 +40,44 @@ void setup()
   softwareSerial.begin(57600);
 
   pinMode(EN_PIN, OUTPUT);
-  digitalWrite(EN_PIN, LOW);
+  pinMode(DIAG_PIN, INPUT);
 
   driver.begin();
 
-  driver.toff(5);
-  driver.microsteps(16);
+  driver.toff(4);
+  driver.blank_time(24);
+  driver.microsteps(MICROSTEPS);
   driver.rms_current(RMS_CURRENT);
-  driver.VACTUAL(1000);
+
+// STALLGUARD CONFIG
+  driver.pwm_autoscale(true); 
+  driver.semin(5);
+  driver.semax(2);
+  driver.sedn(0b01);
+  driver.TCOOLTHRS(0xFFFFF); 
+  driver.SGTHRS(STALL_VALUE);
+  
+  digitalWrite(EN_PIN, LOW);
+  attachInterrupt(digitalPinToInterrupt(DIAG_PIN), onStall, RISING);
+  
+  driver.VACTUAL(150);
 }
 
-bool shaft = false;
 
 void loop()
 {
-  Serial.printf("Conn %d, Version %d, Microsteps: %d\n", driver.test_connection(), driver.version(), driver.microsteps());
-  delay(1000);
-  shaft = !shaft;
-  driver.shaft(shaft);
+  Serial.printf("SG: %d. DIAG: %d\n", driver.SG_RESULT(), digitalRead(DIAG_PIN));
+  //Serial.printf("Conn %d, Version %d, Microsteps: %d\n", driver.test_connection(), driver.version(), driver.microsteps());
+  if (interrupted || digitalRead(DIAG_PIN) == HIGH) {
+    interrupted = false;
+    Serial.println("Interrupted!");
+    shaft = !shaft;
+    driver.shaft(shaft);
+    // Reset the interrupt
+    digitalWrite(EN_PIN, HIGH);
+    delay(10);
+    digitalWrite(EN_PIN, LOW);
+  }
+  delay(100);
+
 }
